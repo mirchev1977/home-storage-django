9c7d7b8ac31e26937cc90febf800126ea129c51a
Miroslav <mirchev1977@yahoo.com>
2020-12-11 05:44:09 +0200

    added phone and email

--- app/models.py
+++ app/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
+    phone = models.CharField(max_length=255)
